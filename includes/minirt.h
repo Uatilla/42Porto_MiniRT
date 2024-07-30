e72a755e522a3dc3520debc6ff2fa0e6a2d55b21
Uatilla Viana Almeida <uviana-a@c2r5s1.42porto.com>
2024-07-30 20:47:48 +0100

    Details

--- includes/minirt.h
+++ includes/minirt.h
@@ -13,8 +13,17 @@
 #ifndef MINIRT_H
 # define MINIRT_H
 
+//LIBRARIES
 # include <stdio.h>
 # include "../libraries/minilibx-linux/mlx.h"
 # include "../libraries/libft/libft.h"
+
+//ERROR MESSAGES
+
+//STRUCTURES
+
+//MACROS
+
+//SOURCES
 
 #endif
