6cc8f2da402fe6180be4b32b0ce8edc78029a3f7
Jburlama <jhonas.burlamaqui@gmail.com>
2024-08-03 16:49:04 +0100

    shirink main

--- srcs/main.c
+++ srcs/main.c
@@ -12,51 +12,8 @@
 
 #include "../includes/minirt.h"
 
-/// @brief Find the intersection (scalar value) in a vector.
-/// @param a Tuple to be multiplied.
-/// @param sc Scalar value.
-/// @return The tuple(point) in the scalar intersection.
-/*t_tuple mult_tuple_scalar(t_tuple a, float sc)
-{
-    t_tuple res;
-
-    res.x = a.x + b.x;
-    res.y = a.y + b.y;
-    res.z = a.z + b.z;
-    if (a.w != 999999 && b.w != 999999)
-        res.w = a.w + b.w;
-    else
-        res.w = 999999;
-    return(res);
-}*/
-
 int main(int argc, char **argv)
 {
-    //t_minirt    mrt;
-    int         fd;
-    t_point   p;
-    t_point   p2;
-    t_vector  v;
-    t_vector  v2;
-    t_color   c;
-    //t_point   res;
-
-    fd = chk_input(argc, argv[1]);
-    
-    p = creating_point(1, -2, 3);
-    printf("P: X(%f) Y(%f) Z(%f) W(%f)\n", p.x, p.y, p.z, p.w);
-    p2 = creating_point(5, 6, 7);
-    printf("P2: X(%f) Y(%f) Z(%f) W(%f)\n", p2.x, p2.y, p2.z, p2.w);
-    printf("----------------\n");
-    v = creating_vector(3, 2, 1);
-    printf("V: X(%f) Y(%f) Z(%f) W(%f)\n", v.x, v.y, v.z, v.w);
-    v2 = creating_vector(5, 6, 7);
-    printf("V2: X(%f) Y(%f) Z(%f) W(%f)\n", v2.x, v2.y, v2.z, v2.w);
-    printf("----------------\n");
-    c = creating_color(200, 113, 134);
-    printf("C: X(%f) Y(%f) Z(%f) W(%f)\n", c.x, c.y, c.z, c.w);
-    printf("----------------\n");
-    //res = mult_tuple_scalar(p, 3.5);
-    //printf("RES: X(%f) Y(%f) Z(%f) W(%f)\n", res.x, res.y, res.z, res.w);
+	t_minirt	data;
     return (0);
 }
