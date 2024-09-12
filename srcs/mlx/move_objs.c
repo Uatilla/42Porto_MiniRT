7bb8f3cdd5407939228d4e6b593f2e731bdb5245
Uatilla Viana Almeida <uviana-a@c2r5s1.42porto.com>
2024-09-12 21:50:10 +0100

    Working on View transformation to move camera 2D

--- srcs/mlx/move_objs.c
+++ srcs/mlx/move_objs.c
@@ -93,9 +93,11 @@ void	move_camera(t_minirt *win, int key)
 		camera->center.z = camera->center.z - 1;
 	else if (key == KEY_MINUS)
 		camera->center.z = camera->center.z + 1;
+	clean_matrix(NULL, win->camera.trans, 0);	
 	win->camera.trans = view_transformation(&win->camera.center, &(t_point){0, 0, 0, 1}, &(t_vector){0, 1, 0, 0});
+	//win->camera.trans = view_transformation(&(t_point){0, 0, -20, 1}, &win->camera.center, &(t_vector){0, 1, 0, 0});
+	clean_matrix(NULL, win->camera.inver, 0);
 	win->camera.inver = mtx_inverse(win, win->camera.trans);
-	
 }
 
 /// @brief Move the obj and sets the mlx to display the new image.
@@ -107,8 +109,8 @@ void	move_win(t_minirt *win, int key)
 
 	if (win->world.objs && win->world.scene_elem == OBJECT)
 		move_obj(&win->world, key, win->world.obj_selected);
-	//else if (win->world.scene_elem == CAMERA)
-	//	move_camera(win, key);
+	else if (win->world.scene_elem == CAMERA)
+		move_camera(win, key);
 	else if (win->world.scene_elem == LIGHT)
 		move_light(&win->world, key);
 	mlx_destroy_image(win->canvas.mlx, win->canvas.img);
--- srcs/objects/parse_render_settings.c
+++ srcs/objects/parse_render_settings.c
@@ -38,12 +38,10 @@ void	parse_camera(t_minirt *mrt, char **line)
 	t_point	camera_center;
 	t_vector	norm_vect;
 
+	mrt->camera = camera_construct(WIDTH, HEIGTH, degree_to_rad(ft_atof(line[3])));
 	camera_center = get_tuple(line[1], 1);
 	mrt->camera.center = camera_center;
-
 	norm_vect = get_tuple(line[2], 0);
-
-	mrt->camera = camera_construct(WIDTH, HEIGTH, degree_to_rad(ft_atof(line[3])));
 	mrt->camera.trans = view_transformation(&camera_center, &(t_point){0, 0, 0, 1}, &norm_vect);
 	mrt->camera.inver = mtx_inverse(mrt, mrt->camera.trans);
 }
